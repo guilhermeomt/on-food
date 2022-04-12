e7d2d9ed79cbf8c9ed7344a0f6ac0c277fe7e2b6
Guilherme <guilhermet213@gmail.com>
2022-04-12 00:13:34 -0300

    feat: add Footer and Header components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import { Header } from '@/components'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Header />
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
