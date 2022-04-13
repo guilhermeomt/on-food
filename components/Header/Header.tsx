d011b9a0c85cff50050d1ef83a4596235b313572
Guilherme <guilhermet213@gmail.com>
2022-04-13 08:14:42 -0300

    feat: add styles to Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+import Styles from "./Header.module.css";
+import { IconBookmark, IconHome } from "@/icons";
+
 export function Header() {
   return (
     <header>
-      <h1>FrontExpert</h1>
+      <nav className={Styles.nav}>
+        <div className={Styles.brand}>
+          <Link href="/">
+            <a>
+              <Image
+                src="/images/logo.png"
+                alt="OnFood "
+                width="45"
+                height="36"
+              />
+              <span>OnFood</span>
+            </a>
+          </Link>
+        </div>
+        <ul className="flex">
+          <li>
+            <Link href="/">
+              <a>
+                <IconHome />
+                Home
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/favoritos">
+              <a>
+                <IconBookmark />
+                Favoritos
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,6 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
     </div>
   )
 }
