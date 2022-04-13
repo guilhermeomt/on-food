4cd9342371b706ec82410e299f7fbbaa98c2f42f
Guilherme <guilhermet213@gmail.com>
2022-04-13 08:31:36 -0300

    feat: add styles to Footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
+import Styles from "./Footer.module.css";
+
 export function Footer() {
   return (
     <footer>
-      FrontExpert
+      <div className={Styles.footer}>
+        <p>&copy; 2022 - Imersão FrontExpert</p>
+      </div>
     </footer>
   );
 }
