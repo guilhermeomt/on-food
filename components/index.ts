56b8398aa1b2d69d5013204e79ebd9387375ce76
Guilherme <guilhermet213@gmail.com>
2022-04-14 01:49:04 -0300

    feat: add DisheList component

--- components/index.ts
+++ components/index.ts
@@ -5,4 +5,5 @@ export * from "./DisheDetails";
 export * from "./Dishe";
 export * from "./Search";
 export * from "./Character";
+export * from './DisheList';
 // PLOP WILL AUTO-EXPORT YOUR COMPONENT HERE. DO NOT REMOVE
