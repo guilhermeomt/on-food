1d55de00b73d318e641bcb9c33ffb7b24ba44e9f
Guilherme <guilhermet213@gmail.com>
2022-04-14 01:28:27 -0300

    feat: add Search component

--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,5 @@ export * from "./Header";
 export * from "./PageTemplate";
 export * from "./DisheDetails";
 export * from "./Dishe";
+export * from "./Search";
 // PLOP WILL AUTO-EXPORT YOUR COMPONENT HERE. DO NOT REMOVE
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import { Dishe, PageTemplate } from "@/components";
+import { PageTemplate, Search } from "@/components";
 import type { NextPage } from "next";
+import Styles from "../styles/internals.module.css";
 
 const Home: NextPage = () => {
   return (
     <>
       <PageTemplate>
-        <h1>Encontre opções em Delivery próximos à você.</h1>
-        <h2>
-          Descubra seu novo Delivery favorito. Milhares de opções a um clique.
-        </h2>
-        <Dishe />
+        <div className={Styles.content}>
+          <h1>Encontre opções em Delivery próximos à você.</h1>
+          <Search />
+          <h2>
+            Descubra seu novo Delivery favorito. Milhares de opções a um clique.
+          </h2>
+        </div>
       </PageTemplate>
     </>
   );
