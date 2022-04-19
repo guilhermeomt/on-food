3652e204f11f67029d2d4570c9c4ded04ea55b23
Guilherme <guilhermet213@gmail.com>
2022-04-18 22:16:16 -0300

    feat: add skeletons on loading

--- components/DishList/DishList.tsx
+++ components/DishList/DishList.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { Dish } from "@/components";
+import { Dish, DishSkeleton } from "@/components";
 import { api } from "@/services";
 import { DishType } from "@/types";
 import Styles from "./DishList.module.css";
@@ -12,10 +12,12 @@ export function DishList() {
 
   const { data, isFetching } = useQuery<DishType[]>("dishes", fetchDishes);
 
+  const skeletons = Array.from({ length: 3 });
+
   return (
     <div className={Styles.list}>
       {isFetching
-        ? "Carregando..."
+        ? skeletons.map((_, index) => <DishSkeleton key={index} />)
         : data?.map((dish) => <Dish key={dish.id} {...dish} />)}
     </div>
   );
