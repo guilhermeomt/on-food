af0f0ea9b42bc967e018d489ad78d4c38b1563cb
Guilherme <guilhermet213@gmail.com>
2022-04-19 20:37:39 -0300

    fix: api url

--- core/services/api.ts
+++ core/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://api.imersaofx.danieldcs.com",
+  baseURL: "https://api.imersaofx.danieldcs.com",
   headers: {
     "Content-Type": "application/json",
     Authorization: "Bearer Z3VpbGhlcm1ldDIxM0BnbWFpbC5jb20=",
